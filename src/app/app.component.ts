72bd24000826eeaa9b0eda0d982589500e5068af
hararm <avarmenhg@yahoo.com>
2017-05-06 18:37:23 -0400

    some transitions added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,43 @@
-import { Component } from '@angular/core';
-import { state, style, trigger } from '@angular/animations';
+import {Component} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [
-    trigger('clickedState',[
+    trigger('clickedState', [
       state('default', style({
         backgroundColor: 'orange',
         width: '100px',
         height: '100px'
-        })),
+      })),
       state('clicked', style({
         backgroundColor: 'blue',
         width: '300px',
         height: '50px'
-      }))
+      })),
+      state('mouseDown', style({
+        backgroundColor: 'red',
+        border: '1px solid black',
+        width: '100px',
+        height: '100px'
+      })),
+      transition('default => clicked', animate('1000ms 500ms ease-in')),
+      transition('clicked => default', animate(300)),
+      transition('mouseDown <=> clicked', animate(300))
     ])
   ]
 })
 export class AppComponent {
-  title = 'app works!';
+  clickInfo = 'default';
+
+  onClick() {
+    this.clickInfo = 'clicked';
+    setTimeout(() => {
+      this.clickInfo = 'default';
+    }, 3000);
+  }
 }
+
+
