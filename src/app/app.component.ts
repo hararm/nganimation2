8419097620dac8358c6989d3894e12babdfbb21d
armenh <avarmenhg@gmail.com>
2017-05-05 21:16:45 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { state, style, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('clickedState',[
+      state('default', style({
+        backgroundColor: 'orange',
+        width: '100px',
+        height: '100px'
+        })),
+      state('clicked', style({
+        backgroundColor: 'blue',
+        width: '300px',
+        height: '50px'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'app works!';
